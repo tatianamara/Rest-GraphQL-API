6028b453a85a1a6d272502ebfbd47cfc842d6a07
tatianamara <tatiana.mara@rockcontent.com>
2020-10-04 13:20:25 -0300

    Static images and error handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{
@@ -17,26 +19,33 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
     const error = validationResult(req);
-    if(!error.isEmpty()){
-        return res.status(422).json({
-            message: 'Validation failed, entered data is incorrect.',
-            errors: error.array()
-        });
+    if (!error.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.title;
-    // Create post in db
-
-    res.status(201).json({
-        message: 'Post created successfuly!',
-        post: {
-            id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: 'Tatiana'
-            },
-            createdAt: new Date()
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/download (2).jpg',
+        creator: {
+            name: 'Tatiana'
         }
     });
+    post.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Post created successfuly!',
+                post: result
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
