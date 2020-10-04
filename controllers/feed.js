d012e8a4319d96fcf73d364c51a2564e48080767
tatianamara <tatiana.mara@rockcontent.com>
2020-10-04 13:59:34 -0300

    Uploading images

--- controllers/feed.js
+++ controllers/feed.js
@@ -26,12 +26,18 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
+    if (!req.file){
+        const error = new Error('No image provided.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const imageUrl = req.file.path.replace("\\" ,"/");
     const title = req.body.title;
     const content = req.body.title;
     const post = new Post({
         title: title,
         content: content,
-        imageUrl: 'images/download (2).jpg',
+        imageUrl: imageUrl,
         creator: {
             name: 'Tatiana'
         }
