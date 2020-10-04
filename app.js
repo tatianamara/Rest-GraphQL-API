8d9ca4c2c01174120976675213d5e3aa3364a9ea
tatianamara <tatiana.mara@rockcontent.com>
2020-10-04 12:53:46 -0300

    Setting up a post model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -16,5 +17,10 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
-
-app.listen(8080);
+mongoose.connect(
+    'mongodb+srv://mongo_tatiana:<password>@cluster0.1y1dx.mongodb.net/messages?retryWrites=true'
+)
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
