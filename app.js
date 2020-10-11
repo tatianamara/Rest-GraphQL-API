f9e1df815500fa711bd3a741f00e2713575e8f89
tatianamara <tatiana.mara@rockcontent.com>
2020-10-11 13:09:34 -0300

    Working with authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -40,18 +41,19 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allowing access from all sources
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allowing access to this methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS'); // Allowing access to this methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow customers to send extra authorization information and content type in headers
     next();
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({ message: message });
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose.connect(
