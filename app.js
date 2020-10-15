c564017b811ef8dea5f2c642b3788aaa60f91436
tatianamara <tatiana.mara@rockcontent.com>
2020-10-15 14:48:12 -0300

    Using websockets and socket.io to have real time connections

--- app.js
+++ app.js
@@ -60,6 +60,10 @@ mongoose.connect(
     'mongodb+srv://mongo_tatiana:<password>@cluster0.1y1dx.mongodb.net/messages?retryWrites=true'
 )
     .then(result => {
-        app.listen(8080);
+        const server = app.listen(8080);
+        const io = require('./socket').init(server);
+        io.on('connection', socket => {
+            console.log('Client Connected!');
+        });
     })
     .catch(err => console.log(err));
